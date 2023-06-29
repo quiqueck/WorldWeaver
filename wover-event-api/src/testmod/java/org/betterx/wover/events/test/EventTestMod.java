37836285ac123c301012ca1cb2f110d9c3348110
Frank <developer@ambertation.de>
2023-06-30 01:14:09 +0200

    Added testmod to event api

---
+++ wover-event-api/src/testmod/java/org/betterx/wover/events/test/EventTestMod.java
@@ -0,0 +1,13 @@
+package org.betterx.wover.events.test;
+
+import org.betterx.wover.core.api.ModCore;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EventTestMod implements ModInitializer {
+    public static final ModCore C = ModCore.create("event-test-mod");
+
+    @Override
+    public void onInitialize() {
+    }
+}
