009d6ebfeb5939d348ffd160fcdc03807ac2eb6a
Frank <developer@ambertation.de>
2023-06-30 01:13:43 +0200

    Added stub for surface api

---
+++ wover-surface-api/src/main/java/org/betterx/wover/surface/impl/WoverSurface.java
@@ -0,0 +1,15 @@
+package org.betterx.wover.surface.impl;
+
+
+import org.betterx.wover.core.api.ModCore;
+
+import net.fabricmc.api.ModInitializer;
+
+public class WoverSurface implements ModInitializer {
+    public static final ModCore C = ModCore.create("wover-surface");
+
+    @Override
+    public void onInitialize() {
+        
+    }
+}
