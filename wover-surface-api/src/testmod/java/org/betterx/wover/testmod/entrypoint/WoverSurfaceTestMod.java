603ddf8b34fc255f567243b5d1e92d283cfd2a9d
Frank <developer@ambertation.de>
2023-07-02 12:50:45 +0200

    Added client testmod

--- wover-surface-api/src/testmod/java/org/betterx/wover/testmod/entrypoint/WoverSurfaceTestMod.java
+++ wover-surface-api/src/testmod/java/org/betterx/wover/testmod/entrypoint/WoverSurfaceTestMod.java
@@ -1,10 +1,14 @@
 package org.betterx.wover.testmod.entrypoint;
 
+import org.betterx.wover.core.api.ModCore;
+
 import net.fabricmc.api.ModInitializer;
 
 public class WoverSurfaceTestMod implements ModInitializer {
+    public static final ModCore C = ModCore.create("wover-surface-testmod");
+
     @Override
     public void onInitialize() {
-        
+
     }
 }
