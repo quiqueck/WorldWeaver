57affd4f9815a4eb604f6dbb720dc7fc216073b0
Frank <developer@ambertation.de>
2023-07-02 12:45:57 +0200

    Added module for common interfaces

---
+++ wover-common-api/src/client/java/org/betterx/wover/entrypoint/client/WoverCommonClient.java
@@ -0,0 +1,10 @@
+package org.betterx.wover.entrypoint.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WoverCommonClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ wover-common-api/src/testmod/java/org/betterx/wover/testmod/entrypoint/WoverCommonTestMod.java
@@ -0,0 +1,10 @@
+package org.betterx.wover.testmod.entrypoint;
+
+import net.fabricmc.api.ModInitializer;
+
+public class WoverCommonTestMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
