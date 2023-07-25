f11ecc6e75fb92a33ce887d694bd10f11cb5e8f2
Frank <developer@ambertation.de>
2023-07-25 23:44:04 +0200

    Added biome api project

---
+++ wover-biome-api/src/main/java/org/betterx/wover/entrypoint/WoverBiome.java
@@ -0,0 +1,14 @@
+package org.betterx.wover.entrypoint;
+
+import org.betterx.wover.core.api.ModCore;
+
+import net.fabricmc.api.ModInitializer;
+
+public class WoverBiome implements ModInitializer {
+    public static final ModCore C = ModCore.create("wover-biome", "wover");
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ wover-biome-api/src/testmodClient/java/org/betterx/wover/testmod/entrypoint/client/WoverBiomeClientTestMod.java
@@ -0,0 +1,10 @@
+package org.betterx.wover.testmod.entrypoint.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WoverBiomeClientTestMod implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
