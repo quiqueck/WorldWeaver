9a454e072acd4129f9289c83f48708dbe34a71a6
Frank <developer@ambertation.de>
2023-07-26 22:50:40 +0200

    Removed test line

--- src/client/java/org/betterx/wover/screens/impl/MainMenu.java
+++ src/client/java/org/betterx/wover/screens/impl/MainMenu.java
@@ -1,6 +1,5 @@
 package org.betterx.wover.screens.impl;
 
-import de.ambertation.wunderlib.WunderLibClient;
 import de.ambertation.wunderlib.ui.vanilla.ConfigScreen;
 import org.betterx.wover.config.api.Configs;
 import org.betterx.wover.config.api.client.ClientConfigs;
@@ -16,7 +15,6 @@ public class MainMenu extends ConfigScreen {
             @Nullable Screen parent
     ) {
         super(parent, Component.translatable("wover.mainmenu.title"), List.of(ClientConfigs.CLIENT, Configs.MAIN));
-        WunderLibClient.shoudlRender = true;
     }
 
     @Override
