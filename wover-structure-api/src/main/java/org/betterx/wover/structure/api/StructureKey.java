ec584b705dc559265c8e7a2d442aa6b835446641
Frank <developer@ambertation.de>
2023-08-13 11:09:28 +0200

    Some Docs

--- wover-structure-api/src/main/java/org/betterx/wover/structure/api/StructureKey.java
+++ wover-structure-api/src/main/java/org/betterx/wover/structure/api/StructureKey.java
@@ -18,6 +18,14 @@ import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * Represents a {@link Structure} that can be registered. This is a primarily a wrapper around
+ * {@link ResourceKey} to make it easier to register structures. But it also provides a function
+ * ({@link #bootstrap(BootstapContext)}) to build and register a structure.
+ *
+ * @param <S> The {@link Structure} type
+ * @param <T> The {@link BaseStructureBuilder} type
+ */
 public abstract class StructureKey<S extends Structure, T extends BaseStructureBuilder<S, T>> {
     /**
      * The key for the {@link Structure} you can use to reference it.
@@ -26,16 +34,29 @@ public abstract class StructureKey<S extends Structure, T extends BaseStructureB
     public final ResourceKey<Structure> key;
 
     @Nullable
-    protected TagKey<Biome> biomeTag;
-    
+    private TagKey<Biome> biomeTag;
+
     @NotNull
     private GenerationStep.Decoration decoration;
 
+    /**
+     * Define a BiomeTag that will be used to determine if the structure can spawn in a biome. You can
+     * build a new Structure tag by using {@link TagManager#BIOMES#makeStructureTag(ResourceLocation)}.
+     *
+     * @param biomeTag The biome tag to set
+     * @return The same instance for chaining
+     */
     public StructureKey<S, T> setBiomeTag(@Nullable TagKey<Biome> biomeTag) {
         this.biomeTag = biomeTag;
         return this;
     }
 
+    /**
+     * Returns the BiomeTag that was set for this structure. If no tag was set, it will return
+     * a new tag that is created from the structure key.
+     *
+     * @return The biome tag
+     */
     @NotNull
     public TagKey<Biome> getBiomeTag() {
         if (biomeTag == null) {
@@ -78,7 +99,19 @@ public abstract class StructureKey<S extends Structure, T extends BaseStructureB
         this.decoration = GenerationStep.Decoration.SURFACE_STRUCTURES;
     }
 
+    /**
+     * The {@link StructureType} for this {@link Structure}. The type is primarily needed internally
+     * to register the structure. If you implement a new Structure class (that inherits from {@link Structure},
+     * you will probably want to return this type from {@link Structure#type()}.
+     */
     public abstract StructureType<S> type();
 
+    /**
+     * Creates a structure builder for the Type. You may use the builder to register the structure in a
+     * {@link BootstapContext} or create an inline Holder for the Structure.
+     *
+     * @param context The bootstrap context
+     * @return The builder
+     */
     public abstract T bootstrap(BootstapContext<Structure> context);
 }
