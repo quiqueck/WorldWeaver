e6768462128e2039ac32527956f58c9d0ee09111
Frank <developer@ambertation.de>
2023-09-12 17:54:29 +0200

    Added register method for Flat Level Presets

--- wover-preset-api/src/main/java/org/betterx/wover/preset/api/flat/FlatLevelPresetManager.java
+++ wover-preset-api/src/main/java/org/betterx/wover/preset/api/flat/FlatLevelPresetManager.java
@@ -4,9 +4,18 @@ import org.betterx.wover.events.api.Event;
 import org.betterx.wover.preset.api.event.OnBootstrapFlatLevelPresets;
 import org.betterx.wover.preset.impl.flat.FlatLevelPresetManagerImpl;
 
+import net.minecraft.data.worldgen.BootstapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.levelgen.flat.FlatLayerInfo;
 import net.minecraft.world.level.levelgen.flat.FlatLevelGeneratorPreset;
+import net.minecraft.world.level.levelgen.structure.StructureSet;
+
+import java.util.Set;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Utility class for {@link FlatLevelGeneratorPreset}s.
@@ -33,5 +42,39 @@ public class FlatLevelPresetManager {
      */
     public static ResourceKey<FlatLevelGeneratorPreset> createKey(ResourceLocation loc) {
         return FlatLevelPresetManagerImpl.createKey(loc);
+    }
+
+    /**
+     * Registers a {@link FlatLevelGeneratorPreset}.
+     *
+     * @param ctx                  The {@link BootstapContext} to register the preset in.
+     * @param presetKey            The key of the preset.
+     * @param icon                 The icon of the preset.
+     * @param biomeKey             The key of the biome to use.
+     * @param allowedStructureSets The allowed structure sets.
+     * @param addDecorations       Whether to add decorations.
+     * @param addLakes             Whether to add lakes.
+     * @param flatLayerInfos       The flat layer infos.
+     */
+    public static void register(
+            @NotNull BootstapContext<FlatLevelGeneratorPreset> ctx,
+            @NotNull ResourceKey<FlatLevelGeneratorPreset> presetKey,
+            @NotNull ItemLike icon,
+            @NotNull ResourceKey<Biome> biomeKey,
+            @Nullable Set<ResourceKey<StructureSet>> allowedStructureSets,
+            boolean addDecorations,
+            boolean addLakes,
+            FlatLayerInfo... flatLayerInfos
+    ) {
+        FlatLevelPresetManagerImpl.register(
+                ctx,
+                presetKey,
+                icon,
+                biomeKey,
+                allowedStructureSets == null ? Set.of() : allowedStructureSets,
+                addDecorations,
+                addLakes,
+                flatLayerInfos
+        );
     }
 }
--- wover-preset-api/src/main/java/org/betterx/wover/preset/impl/flat/FlatLevelPresetManagerImpl.java
+++ wover-preset-api/src/main/java/org/betterx/wover/preset/impl/flat/FlatLevelPresetManagerImpl.java
@@ -5,12 +5,23 @@ import org.betterx.wover.events.impl.EventImpl;
 import org.betterx.wover.preset.api.context.FlatLevelPresetBootstrapContext;
 import org.betterx.wover.preset.api.event.OnBootstrapFlatLevelPresets;
 
+import net.minecraft.core.HolderGetter;
+import net.minecraft.core.HolderSet;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.levelgen.flat.FlatLayerInfo;
 import net.minecraft.world.level.levelgen.flat.FlatLevelGeneratorPreset;
+import net.minecraft.world.level.levelgen.flat.FlatLevelGeneratorSettings;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.minecraft.world.level.levelgen.structure.StructureSet;
 
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 import org.jetbrains.annotations.ApiStatus;
 
 public class FlatLevelPresetManagerImpl {
@@ -32,5 +43,49 @@ public class FlatLevelPresetManagerImpl {
     private static void onBootstrap(BootstapContext<FlatLevelGeneratorPreset> context) {
         FlatLevelPresetBootstrapContext ctx = new FlatLevelPresetBootstrapContext(context);
         BOOTSTRAP_FLAT_LEVEL_PRESETS.emit(c -> c.bootstrap(ctx));
+    }
+
+    public static void register(
+            BootstapContext<FlatLevelGeneratorPreset> ctx,
+            ResourceKey<FlatLevelGeneratorPreset> presetKey,
+            ItemLike icon,
+            ResourceKey<Biome> biomeKey,
+            Set<ResourceKey<StructureSet>> allowedStructureSets,
+            boolean addDecorations,
+            boolean addLakes,
+            FlatLayerInfo... flatLayerInfos
+    ) {
+        final HolderGetter<StructureSet> structureSets = ctx.lookup(Registries.STRUCTURE_SET);
+        Objects.requireNonNull(structureSets);
+
+        final HolderGetter<PlacedFeature> placedFeatures = ctx.lookup(Registries.PLACED_FEATURE);
+        final HolderGetter<Biome> biomes = ctx.lookup(Registries.BIOME);
+
+        HolderSet.Direct<StructureSet> structures = HolderSet.direct(
+                allowedStructureSets.stream()
+                                    .map(structureSets::getOrThrow)
+                                    .toList());
+
+        FlatLevelGeneratorSettings flatLevelGeneratorSettings = new FlatLevelGeneratorSettings(
+                Optional.of(structures),
+                biomes.getOrThrow(biomeKey),
+                FlatLevelGeneratorSettings.createLakesList(placedFeatures)
+        );
+        if (addDecorations) {
+            flatLevelGeneratorSettings.setDecoration();
+        }
+
+        if (addLakes) {
+            flatLevelGeneratorSettings.setAddLakes();
+        }
+
+        for (int i = flatLayerInfos.length - 1; i >= 0; --i) {
+            flatLevelGeneratorSettings.getLayersInfo().add(flatLayerInfos[i]);
+        }
+
+        ctx.register(
+                presetKey,
+                new FlatLevelGeneratorPreset(icon.asItem().builtInRegistryHolder(), flatLevelGeneratorSettings)
+        );
     }
 }
