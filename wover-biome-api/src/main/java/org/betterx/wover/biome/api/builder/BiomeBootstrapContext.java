0e1959a3fd150a41fe040ab8ec1189c7a4c0579b
Frank <developer@ambertation.de>
2023-09-14 20:43:04 +0200

    Addec correct interface

--- wover-biome-api/src/main/java/org/betterx/wover/biome/api/builder/BiomeBootstrapContext.java
+++ wover-biome-api/src/main/java/org/betterx/wover/biome/api/builder/BiomeBootstrapContext.java
@@ -1,5 +1,7 @@
 package org.betterx.wover.biome.api.builder;
 
+import org.betterx.wover.core.api.registry.LookupProvider;
+
 import com.mojang.serialization.Lifecycle;
 import net.minecraft.core.HolderGetter;
 import net.minecraft.core.Registry;
@@ -7,7 +9,7 @@ import net.minecraft.resources.ResourceKey;
 
 import org.jetbrains.annotations.NotNull;
 
-public interface BiomeBootstrapContext {
+public interface BiomeBootstrapContext extends LookupProvider {
     void register(@NotNull BiomeBuilder<?> builder, Lifecycle lifecycle);
     void register(@NotNull BiomeBuilder<?> builder);
     <S> HolderGetter<S> lookup(@NotNull ResourceKey<? extends Registry<? extends S>> registryKey);
